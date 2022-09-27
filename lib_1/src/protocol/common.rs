cf939aadce52f6103dec5b8b7b110c4447bb15ef
fvilante <fvilante1@gmail.com>
2022-09-27 19:30:14 -0300

    test: Add unit test

--- lib_1/src/protocol/common.rs
+++ lib_1/src/protocol/common.rs
@@ -5,24 +5,6 @@ pub const NACK: u8 = 0x15;
 pub const ETX: u8 = 0x03;
 
 
-#[derive(Debug,Copy,Clone,PartialEq)]
-pub struct Frame(pub u8, pub u8, pub u8, pub u8);
-
-impl Frame {
-    pub fn to_array(self) -> [u8;4] {
-        let Frame(d0, d1, d2, d3) = self;
-        let arr = [d0,d1,d2,d3];
-        return arr
-    }
-
-    pub fn from_array(arr: &[u8;4]) -> Frame {
-        let [ d0, d1, d2, d3 ] = arr;
-        Frame(d0.clone(), d1.clone(), d2.clone(), d3.clone())
-    }
-
-}
-
-
 #[derive(Debug, PartialEq, Copy, Clone)]
 pub enum StartByte {
     STX = 0x02,
@@ -31,17 +13,24 @@ pub enum StartByte {
 }
 
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_frame_works() {
-        let expected: [u8; 4] = [1,2,3,4];
-        let frame = Frame::from_array(&expected);
-        let actual = frame.to_array();
-        assert_eq!(expected, actual);
+    fn it_is_correctly_defined_start_bytes() {
+        assert_eq!(StartByte::STX as u8, 0x02);
+        assert_eq!(StartByte::ACK as u8, 0x06);
+        assert_eq!(StartByte::NACK as u8, 0x15);
+    }
+
+    #[test]
+    fn it_is_correctly_control_bytes() {
+        assert_eq!(ESC, 0x1B);
+        assert_eq!(STX, 0x02);
+        assert_eq!(ACK, 0x06);
+        assert_eq!(NACK, 0x15);
+        assert_eq!(ETX, 0x03);
     }
 }
 
