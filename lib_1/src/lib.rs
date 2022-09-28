5e9119d7d91039e6ad9fa38e81b227177886fae8
fvilante <fvilante1@gmail.com>
2022-09-28 17:46:58 -0300

    refactor: simplification

--- lib_1/src/lib.rs
+++ lib_1/src/lib.rs
@@ -5,17 +5,12 @@ pub mod protocol;
 pub mod types;
 pub mod mock;
 
-pub fn add(left: u8, right: u8) -> u8 {
-    left + right + 2
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 6);
+        assert_eq!(true, true);
     }
 }
