c7a93b8915a86d6d5187018114d80cb0fedf7363
fvilante <fvilante1@gmail.com>
2022-10-04 14:01:36 -0300

    feat: introduced double buffer swap for lcd display

--- avr_main/src/menu/menu_lcd.rs
+++ avr_main/src/menu/menu_lcd.rs
@@ -1,3 +1,5 @@
+use core::borrow::BorrowMut;
+
 use crate::board::output_expander::OutputExpander;
 use crate::board::{lcd, output_expander};
 use crate::board::keyboard::{ Keypad, KeyCode };
@@ -154,7 +156,67 @@ impl Keyboard {
 }
 
 
-struct CursorPosition { col: u8, row: u8 }
+struct Point {
+    x: u8,
+    y: u8,
+}
+
+impl Point {
+    fn new(x: u8, y: u8) -> Self {
+        Self {
+            x,
+            y,
+        }
+    }
+}
+
+
+struct CursorPosition { 
+    point: Point,
+}
+
+impl CursorPosition {
+
+    fn new() -> Self {
+        Self {
+            point: Point::new(0, 0),
+        }
+    }
+
+    fn new_from_point(point: Point) -> Self {
+        Self {
+            point,
+        }
+    }
+
+    fn from_index(index: usize) -> Self {
+        let col: u8 = (index % 40).try_into()./*clamp(0,79).*/unwrap_or(0);
+        let row: u8 = (index / 40).try_into()./*clamp(0,1).*/unwrap_or(0);
+        let point = Point::new(col, row);
+        Self {
+            point,
+        }
+    }
+    
+    fn get_index(&self ) -> usize {
+        let Point{x:col, y:row} = self.point;
+        let index = col + (40*row);
+        index.into()
+    }
+
+    fn set_point(&mut self, point: Point) -> &mut Self {
+        self.point = point;
+        self
+    }
+
+    fn increment(&mut self) -> &mut Self {
+        let mut index = self.get_index();
+        index += 1;
+        let new_point = Self::from_index(index.clamp(0, 79)).point ;
+        self.point = new_point;
+        self
+    }
+}
 
 struct Canvas {
     is_initialized: bool,
@@ -169,27 +231,56 @@ impl Canvas  {
         lcd::lcd_initialize();
         Self {
             is_initialized: true,
-            cursor_position: CursorPosition { col: 0, row: 0 },
+            cursor_position: CursorPosition::new_from_point(Point::new(0,0)),
             screen_buffer_input: ['x' as u8; 80],
             screen_buffer_output: ['x' as u8; 80],
         }
     }
 
-    fn print_char(&self, char: char) {
-        lcd::print_char(char);
+    // input part
+
+    fn print_char(&mut self, char: char) {
+        let index = self.cursor_position.get_index();
+        self.screen_buffer_input[index as usize] = char as u8; //TODO: check if this convertion is safe
+        self.cursor_position.increment();
+        //lcd::print_char(char);
     }
 
 
     fn set_cursor(&mut self, col: u8, row: u8) {
-        //self.cursor_position = CursorPosition{col, row};
-        lcd::setCursor(col, row);
+        self.cursor_position.set_point(Point::new(col, row));
+        //lcd::setCursor(col, row);
     }
 
-    fn clear(&self) {
-        lcd::clear();
+    fn clear(&mut self) {
+        self.screen_buffer_input = ['x' as u8; 80];
+        self.cursor_position.set_point(Point::new(0,0));
+        //lcd::clear();
     }
+
+    // output part
+
+
+    /// The purpose of this routine is to avoid unecessary writings to LCD.
+    /// It swaps two lcd buffers: The output_buffer represents current state of lcd and
+    /// input_buffer represent the desired state of lcd
+    fn swap(&mut self) {
+        for (index, input_byte) in self.screen_buffer_input.iter().enumerate() {
+            let output_byte = self.screen_buffer_output[index].borrow_mut();
+            if input_byte != output_byte {
+                let col: u8 = (index % 40).try_into().unwrap_or(0);
+                let row: u8 = (index / 40).try_into().unwrap_or(0);
+                //write input to screen
+                lcd::setCursor(col, row);
+                lcd::print_u8(*input_byte);
+                //update output buffer
+                *output_byte = *input_byte;
+            
+            }
+        }
+    }
+
 }
-
 
 
 struct Caption<'a> {
@@ -211,7 +302,7 @@ impl<'a> Caption<'a> {
         // do nothing
     }
 
-    fn draw(&self, canvas: &'a Canvas) {
+    fn draw(&self, canvas: &'a mut Canvas) {
         for char_ in self.text.chars() {
             canvas.print_char(char_);
         }
@@ -331,7 +422,7 @@ impl<const SIZE: usize> Field<SIZE> {
         self.blink.update();
     }
 
-    fn draw(&self, canvas: &Canvas) {
+    fn draw(&self, canvas: &mut Canvas) {
         //canvas.clear();
         for (position,digit) in self.buffer.as_array().iter().enumerate() {
             let blink_char = '_';
@@ -358,7 +449,14 @@ pub fn development_entry_point() -> ! {
     // initialization
     let beep = |on:bool| { OutputExpander::new().BUZZER(on).commit(); };
     let mut keyboard = Keyboard::new(beep);
-    let  canvas = Canvas::new();
+    let mut canvas = Canvas::new();
+
+    canvas.print_char('A');
+    canvas.print_char('B');
+
+    canvas.swap();
+
+    //loop { }
     
     //widgets
     let caption = Caption::new("My name is...");
@@ -370,10 +468,12 @@ pub fn development_entry_point() -> ! {
         if let Some(key) = keyboard.get_key() {
             field.send_key(key);
         }
+        canvas.swap();
         field.update();
         canvas.clear();
-        caption.draw(&canvas);
-        field.draw(&canvas);
+        caption.draw(&mut canvas);
+        field.draw(&mut canvas);
+
         // draw: draw the Field
     }
 }
