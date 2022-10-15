a7fcb14c4c9c0df40371494a3614f61a250b79e6
fvilante <fvilante1@gmail.com>
2022-10-15 19:15:20 -0300

    feat: parser of the string of menuitem construction

--- avr_main/src/menu/main.rs
+++ avr_main/src/menu/main.rs
@@ -1,3 +1,6 @@
+use core::str::FromStr;
+
+use alloc::string::ToString;
 use heapless::String;
 use heapless::Vec;
 use crate::board::output_expander::OutputExpander;
@@ -7,6 +10,8 @@ use crate::enviroment::front_panel::FrontPanel;
 use super::flash::FlashString;
 use super::keyboard::Keyboard;
 use super::canvas::Canvas;
+use super::widget::caption::Caption;
+use super::widget::field::Field;
 use super::widget::submenu::Items;
 use super::widget::submenu::SubMenu;
 use crate::menu::widget::widget::Widget;
@@ -15,6 +20,9 @@ use crate::menu::widget::widget::Widget;
 use avr_progmem::progmem;
 
 progmem! {
+
+    static progmem string T0 = "Posicao inicial     ${nnnnn} mm/s";
+
     static progmem string E0 = "Erro de carga de parametro";
     static progmem string S0 = "Posicao Inicial";
     static progmem string S1 = "Posicao Final";
@@ -27,7 +35,56 @@ progmem! {
 
     //NOTE: it is possible to load any type in progmem not only strings
     static progmem A0: [u8; 6] = [0,1,2,3,4,5];
+    static progmem string ERRO_01 = "Erro de construcao de string";
 }
+
+enum MenuItemParsed {
+    PureCaption(String<40>), // [Caption]
+    CaptionWithOneField(String<40>, String<10>, String<10>), // [1st Caption, Field Type, Last Caption]
+}
+
+fn parse_menu_item_constructor_string(declaration: FlashString) -> MenuItemParsed {
+    // example of declaration content = "Posicao inicial     ${nnnnn} mm/s"
+    let s: String<40>  = declaration.to_string().unwrap_or(String::from_str("Error: Small container").unwrap());
+    let begin_token: &[_] = &['$', '{'];
+    let end_token: &[_] = &['}'];
+    match s.find(begin_token) {
+        Some(begin_index) =>  {
+            //1st caption ends in begin_index
+            let x = s.split_at(begin_index+begin_token.len());
+            let first_caption_ = x.0;
+            let first_caption = &first_caption_[0..first_caption_.len()-begin_token.len()];
+            let remain = x.1;
+            match remain.find(end_token) {
+                Some(end_index) => {
+                    let y = remain.split_at(end_index);
+                    let field_type = y.0;
+                    let last_caption_ = y.1;
+                    let last_caption = &last_caption_[end_token.len()..last_caption_.len()];
+                    MenuItemParsed::CaptionWithOneField(
+                        String::from_str(first_caption).unwrap(), 
+                        String::from_str(field_type).unwrap(), 
+                        String::from_str(last_caption).unwrap(),
+                    )
+                }
+
+                None => {
+                    //false open, everything is caption
+                    let caption = s.as_str();
+                    MenuItemParsed::PureCaption(String::from_str(caption).unwrap())
+                }
+            }
+            
+        }
+
+        None => {
+            //caption entire string
+            let caption = s.as_str();
+            MenuItemParsed::PureCaption(String::from_str(caption).unwrap())
+        }
+    }
+}
+
 
 
 pub fn development_entry_point() -> ! {
@@ -41,9 +98,26 @@ pub fn development_entry_point() -> ! {
     // initialization
     let beep = |on:bool| { OutputExpander::new().BUZZER(on).commit(); };
     let mut keyboard = Keyboard::new(beep);
-    let mut canvas = Canvas::new();
+  
+    let parsed = parse_menu_item_constructor_string(FlashString::new(&T0));
+    match parsed {
+        MenuItemParsed::PureCaption(caption) => {
+            lcd::print(caption.as_str());
+        },
+        MenuItemParsed::CaptionWithOneField(first_caption, field_type, last_caption) => {
+            lcd::print("1-");
+            lcd::print(first_caption.as_str());
+            lcd::print("2-");
+            lcd::print(&field_type);
+            lcd::print("2-");
+            lcd::print(last_caption.as_str());
+        },
+    }
+  
+    loop { }
 
-    canvas.render();
+    let mut canvas = Canvas::new();
+    canvas.render();  
     
     //widgets
     let mut items: Items = Vec::new();
