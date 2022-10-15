2bc398531bc8c04822e423f52c2d0ee55ed101d8
fvilante <fvilante1@gmail.com>
2022-10-15 15:45:42 -0300

    refactor: reincorporate external file

--- avr_main/src/menu/main.rs
+++ avr_main/src/menu/main.rs
@@ -12,9 +12,22 @@ use super::widget::submenu::SubMenu;
 use crate::menu::widget::widget::Widget;
 
 
-// Load Some Flash strings
-use core::include;
-include!("ttc3100_strings.rs");
+use avr_progmem::progmem;
+
+progmem! {
+    static progmem string E0 = "Erro de carga de parametro";
+    static progmem string S0 = "Posicao Inicial";
+    static progmem string S1 = "Posicao Final";
+    static progmem string S2 = "Velocidade de Avanco";
+    static progmem string S3 = "Velocidade de Retorno";
+    static progmem string S4 = "Aceleracao de Avanco";
+    static progmem string S5 = "Aceleracao de Retorno";
+    static progmem string S6 = "Start Automatico no Avanco";
+    static progmem string S7 = "Start Automatico no Retorno";
+
+    //NOTE: it is possible to load any type in progmem not only strings
+    static progmem A0: [u8; 6] = [0,1,2,3,4,5];
+}
 
 
 pub fn development_entry_point() -> ! {
