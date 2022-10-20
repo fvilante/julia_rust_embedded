cc7b13f58ad98eb223bcc08263658027391046bb
fvilante <fvilante1@gmail.com>
2022-10-20 11:16:31 -0300

    feat: introduced Ram database

--- avr_main/src/menu/database.rs
+++ avr_main/src/menu/database.rs
@@ -1,10 +1,10 @@
 // menu parameters database
 
-pub struct Acessor<'a, T: Clone> {
+pub struct Accessor<'a, T: Clone> {
     ref_mut: &'a mut T,
 }
 
-impl<'a, T: Clone> Acessor<'a, T> {
+impl<'a, T: Clone> Accessor<'a, T> {
 
     pub fn new(ref_mut: &'a mut T) -> Self {
         Self {
@@ -19,14 +19,15 @@ impl<'a, T: Clone> Acessor<'a, T> {
     pub fn get(&self) -> T {
         (*self.ref_mut).clone()
     }
+
 }
 
 
 pub struct DataBase {
     parameter01: u16,
     parameter02: u16,
-    parameter03: u16,
-    parameter04: u16,
+    parameter03: bool,
+    parameter04: u8,
 }
 
 impl DataBase {
@@ -35,24 +36,24 @@ impl DataBase {
         Self {
             parameter01: 1,
             parameter02: 2,
-            parameter03: 3,
+            parameter03: false,
             parameter04: 4,
         }
     }
 
-    pub fn parameter_01(&mut self) -> Acessor<u16> {
-        Acessor::new(&mut self.parameter01)
+    pub fn parameter_01(&mut self) -> Accessor<u16> {
+        Accessor::new(&mut self.parameter01)
     }
 
-    pub fn parameter_02(&mut self) -> Acessor<u16> {
-        Acessor::new(&mut self.parameter02)
+    pub fn parameter_02(&mut self) -> Accessor<u16> {
+        Accessor::new(&mut self.parameter02)
     }
 
-    pub fn parameter_03(&mut self) -> Acessor<u16> {
-        Acessor::new(&mut self.parameter03)
+    pub fn parameter_03(&mut self) -> Accessor<bool> {
+        Accessor::new(&mut self.parameter03)
     }
 
-    pub fn parameter_04(&mut self) -> Acessor<u16> {
-        Acessor::new(&mut self.parameter04)
+    pub fn parameter_04(&mut self) -> Accessor<u8> {
+        Accessor::new(&mut self.parameter04)
     }
 }
