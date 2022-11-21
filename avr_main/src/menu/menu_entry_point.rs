2b465d94b730ac2e7d696d0476a1c872114f0cb3
fvilante <fvilante1@gmail.com>
2022-11-21 12:52:07 -0300

    minor change: created indirect access to static variable through function

--- avr_main/src/menu/menu_entry_point.rs
+++ avr_main/src/menu/menu_entry_point.rs
@@ -82,9 +82,17 @@ progmem! {
 
 
 
-static mut FILE: [u16; 4] = [0x00;4];
-static mut CURSOR: Cursor = Cursor::new(0..4, 0); //TODO: bring text alongside option number in this object ??!
 
+
+fn get_cursor() -> &'static mut Cursor {
+    static mut CURSOR: Cursor = Cursor::new(0..4, 0); //TODO: bring text alongside option number in this object ??!
+    unsafe{ &mut CURSOR }
+}
+
+static mut FILE: [u16; 4] = [0x00;4];
+fn get_file(index: usize) -> &'static mut u16 {
+    unsafe { &mut FILE[index]}
+}
 
 
 
@@ -157,13 +165,13 @@ pub fn development_entry_point() -> ! {
 
     // =========================================================
     let (point1, point2, text) = make_menu_item_helper(1, 33, &POSICAO_INICIAL);
-    let field = make_numerical_field(unsafe{ &mut FILE[0] }, 0, 4, 10..100);
+    let field = make_numerical_field(unsafe{ get_file(2) }, 0, 4, 10..100);
     let mut menu_item = MenuItem::new(point1, text, point2, field, None);
     menu_list.push(menu_item);
 
     // =========================================================
     let (point1, point2, text) = make_menu_item_helper(1, 33, &POSICAO_FINAL);
-    let field = make_numerical_field(unsafe{ &mut FILE[1] }, 0, 4, 0..0xFFFF);
+    let field = make_numerical_field(unsafe{ get_file(1) }, 0, 4, 0..0xFFFF);
     let mut menu_item = MenuItem::new(point1, text, point2, field, None);
     menu_list.push(menu_item);
 
@@ -171,7 +179,7 @@ pub fn development_entry_point() -> ! {
     // =========================================================
     //options
     let (point1, point2, text) = make_menu_item_helper(1, 33, &START_AUTOMATICO_NO_RETORNO);
-    let field = make_optional_field_ligado_desligado(unsafe{ &mut CURSOR }, [&O1, &O2, &O3, &O4]);
+    let field = make_optional_field_ligado_desligado(unsafe{ get_cursor() }, [&O1, &O2, &O3, &O4]);
     let mut menu_item = MenuItem::new(point1, text, point2, field, None);
     menu_list.push(menu_item);
 
