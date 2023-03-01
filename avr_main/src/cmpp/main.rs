a964fad4d56cfdca0e06dbef1dc735e57a5c1ef2
fvilante <fvilante1@gmail.com>
2023-03-01 11:07:00 -0300

    removed old unused code

--- avr_main/src/cmpp/main.rs
+++ avr_main/src/cmpp/main.rs
@@ -1,84 +1,19 @@
-use heapless::Deque;
 use lib_1::protocol::{
-    datalink::{
-        datalink::{word16::Word16, DLError, Datalink},
-        decoder::{Decoder, DecodingError},
-        frame::Frame,
-        prelude::StartByte,
-        transact::{transact, DatalinkError},
-    },
+    datalink::datalink::{DLError, Datalink},
     transport::{
         channel::Channel,
         transport_layer::{
-            self,
-            cmpp_value::{self, MechanicalProperties},
-            new_proposal::Displacement,
-            TLError, TransportLayer,
+            cmpp_value::MechanicalProperties, new_proposal::Displacement, TLError, TransportLayer,
         },
     },
 };
 
 use crate::{
     board::lcd,
-    microcontroler::{
-        delay::{delay_ms, delay_us},
-        serial,
-        timer::now,
-    },
+    microcontroler::{serial, timer::now},
 };
 
-use super::datalink::concrete_serial::ConcreteSerialPort;
 use lib_1::types::serial_connection::SerialConnection;
-
-/// Helper to just clamp values
-fn delay_us_helper(time_us: u64) {
-    let time_us_clamped = time_us.clamp(u32::MIN as u64, u32::MAX as u64);
-    delay_us(time_us_clamped)
-}
-
-fn test_cmpp() {
-    let frame = Frame::make_master_block([0, 0x50, 0, 0].into());
-    let connection = ConcreteSerialPort::new(2400);
-    const timeout_us: u64 = 200 * 1000;
-    let response = transact(frame, connection, timeout_us, delay_us_helper);
-    match response {
-        Ok(_response) => lcd::print("Response Ok"),
-        Err(error) => {
-            lcd::print("Response Err");
-            match error {
-                DatalinkError::DecodingError(error) => {
-                    lcd::print("DecodingError");
-
-                    match error {
-                        DecodingError::InvalidStartByte(start_byte) => {
-                            lcd::print("InvalidStartByte=");
-                            lcd::print_u8_in_hex(start_byte)
-                        }
-                        DecodingError::BufferOverFlow => {
-                            lcd::print("BufferOverFlow");
-                        }
-                        DecodingError::ExpectedEtxOrEscDupButFoundOtherThing(_) => {
-                            lcd::print("ExpectedEtxOrEscDupButFoundOtherThing");
-                        }
-                        DecodingError::ChecksumIsEscButNotDuplicated(_) => {
-                            lcd::print("ChecksumIsEscButNotDuplicated");
-                        }
-                        DecodingError::InvalidChecksum {
-                            expected: _,
-                            received: _,
-                        } => {
-                            lcd::print("InvalidChecksum");
-                        }
-                    }
-                }
-                DatalinkError::ReceptionTimeout { elapsed_time } => {
-                    lcd::print("Timeout=");
-                    let _e = elapsed_time / 1000; //to milisec
-                }
-            }
-        }
-    }
-}
 
 fn panic_error_message(error: TLError) -> () {
     match error {
@@ -110,7 +45,7 @@ pub fn development_entry_point() -> ! {
     let timeout_ms = 1000; // TODO: Maybe in future be calculated as a function of the connection baud rate
 
     let baud_rate = 9600; // FIX: 2400 is not working, the problem seems to be in the register's port setup configuration
-    let serial = serial::init(baud_rate);
+    let _serial = serial::init(baud_rate);
 
     fn try_rx() -> Result<Option<u8>, ()> {
         Ok(serial::try_receive())
