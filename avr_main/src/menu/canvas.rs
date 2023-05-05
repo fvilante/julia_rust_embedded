c724596468e9dde07fe1060bb54d5861dcee9380
fvilante <fvilante1@gmail.com>
2023-05-05 19:53:51 -0300

    refactor simplification on `canvas.rs`

--- avr_main/src/menu/canvas.rs
+++ avr_main/src/menu/canvas.rs
@@ -10,30 +10,25 @@ struct CursorPosition {
 }
 
 impl CursorPosition {
-    fn new() -> Self {
-        Self {
-            point: Point::new(0, 0),
-        }
-    }
-
-    fn new_from_point(point: Point) -> Self {
+    fn new(point: Point) -> Self {
         Self { point }
     }
 
-    fn from_index(index: usize) -> Self {
-        let col: u8 = (index % 40).try_into()./*clamp(0,79).*/unwrap_or(0);
-        let row: u8 = (index / 40).try_into()./*clamp(0,1).*/unwrap_or(0);
+    fn from_index(index: u8) -> Self {
+        let col: u8 = index % 40;
+        let row: u8 = index / 40;
         let point = Point::new(col, row);
         Self { point }
     }
 
-    fn get_index(&self) -> usize {
+    fn get_index(&self) -> u8 {
         let Point { x: col, y: row } = self.point;
         let index = col + (40 * row);
-        index.into()
+        index
     }
 
-    fn set_point(&mut self, point: Point) -> &mut Self {
+    /// Set cursor in the specified point position
+    fn set(&mut self, point: Point) -> &mut Self {
         self.point = point;
         self
     }
@@ -51,57 +46,41 @@ impl CursorPosition {
 ///
 /// Its function is to make possible do cache displayed information reducing
 /// screen flackering. You decide how many frames per second you want to send this to screen through the method [`render`]
-///
-/// For more see: [double buffer](https://en.wikipedia.org/wiki/Multiple_buffering#Double_buffering_in_computer_graphics)
-///
-/// UPDATE:
-/// For memory footprint space conditions we have downgrade this canvas from "double buffer" to "single buffer".
-/// Expected flickering performance is good.
 /// TODO: Rename to ScreenBuffer
 pub struct Canvas {
-    is_initialized: bool,
     cursor_position: CursorPosition, // for screen_buffer_input
     screen_buffer_input: [u8; 80],
-    //screen_buffer_output: [u8; 80],
 }
 
 impl Canvas {
     pub fn new() -> Self {
         lcd::lcd_initialize();
         Self {
-            is_initialized: true,
-            cursor_position: CursorPosition::new_from_point(Point::new(0, 0)),
+            cursor_position: CursorPosition::new(Point::new(0, 0)),
             screen_buffer_input: [' ' as u8; 80],
-            //screen_buffer_output: ['x' as u8; 80],
         }
     }
 
-    // Base input part (These functions are actually what are producing the printing effect on buffer)
-
+    /// Prints one single char on screen and increments cursor to the right (wrapping)
     pub fn print_char(&mut self, char: char) {
         let index = self.cursor_position.get_index();
         self.screen_buffer_input[index as usize] = char as u8; //TODO: check if this convertion is safe
         self.cursor_position.increment();
-        //lcd::print_char(char);
     }
 
+    /// Places cursor at the given position
     pub fn set_cursor(&mut self, point: Point) {
-        self.cursor_position.set_point(point);
-        //let Point { x: col, y: row } = point;
-        //lcd::setCursor(col, row);
+        self.cursor_position.set(point);
     }
 
+    /// Clear all canvas
     pub fn clear(&mut self) {
         const CLEARING_CHAR: u8 = ' ' as u8;
-        for x in &mut self.screen_buffer_input {
-            *x = CLEARING_CHAR;
-        }
-        self.cursor_position.set_point(Point::new(0, 0));
-        //lcd::clear();
+        self.screen_buffer_input.fill(CLEARING_CHAR);
+        self.cursor_position.set(Point::new(0, 0));
     }
 
-    // input part
-
+    /// Prints a `&str`
     pub fn print(&mut self, str: &str) {
         for char in str.chars() {
             self.print_char(char)
