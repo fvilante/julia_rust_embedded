e998216ad3e50a04bb9f208ec46b25dcae06e095
fvilante <fvilante1@gmail.com>
2023-05-16 19:09:40 -0300

    Be specific about `Lcd` driver being exposed

--- avr_main/src/board/lcd.rs
+++ avr_main/src/board/lcd.rs
@@ -4,4 +4,4 @@ mod interface;
 //
 
 /// TODO: Remove the exposition of the driver, use the adapter instead.
-pub use driver::*;
+pub use driver::{clear, lcd_initialize, print, print_u8, set_cursor, NUMBER_OF_LINES};
