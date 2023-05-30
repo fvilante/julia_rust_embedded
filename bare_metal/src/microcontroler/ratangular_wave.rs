e90a030dfc7e19c5d941dd087e3686de45a3a3b7
fvilante <fvilante1@gmail.com>
2023-05-30 13:18:47 -0300

    refactor simplification

--- bare_metal/src/microcontroler/ratangular_wave.rs
+++ bare_metal/src/microcontroler/ratangular_wave.rs
@@ -17,41 +17,32 @@ impl RectangularWave {
     /// interval in miliseconds
     pub fn new(up_interval: Duration, down_interval: Duration) -> Self {
         let initial_state = true;
+        let next_time_point = now() as TimePoint + up_interval as TimePoint;
         Self {
-            up_interval: up_interval.clone(),
+            up_interval,
             down_interval,
-            next_time_point: now() as TimePoint + up_interval as TimePoint,
+            next_time_point,
             current_state: initial_state,
         }
-    }
-
-    /// Restarts the wave form
-    pub fn reset(&mut self) -> &mut Self {
-        let initial_state = true;
-        self.next_time_point = now() as TimePoint + self.up_interval.clone() as TimePoint;
-        self.current_state = initial_state;
-        self
     }
 
     /// Updates the counter
     ///
     /// You should call this method in a frequency relative to the wave form being generated
-    pub fn update(&mut self) -> &mut Self {
-        let is_it_moment_to_change_state = now() as TimePoint > self.next_time_point;
-        if is_it_moment_to_change_state {
-            if self.current_state == true {
-                // up-down edge
-                self.current_state = false;
-                self.next_time_point += self.down_interval.clone() as TimePoint;
+    pub fn update(&mut self) {
+        let is_time_to_change_state = now() as TimePoint > self.next_time_point;
+        if is_time_to_change_state {
+            // toggle state
+            self.current_state = !self.current_state;
+            // calc next time point
+            let delta: TimePoint = if self.current_state {
+                self.up_interval
             } else {
-                // down-up edge
-                self.current_state = true;
-                self.next_time_point += self.up_interval.clone() as TimePoint;
+                self.down_interval
             }
-        } else {
-            // no nothing
+            .into();
+            self.next_time_point += delta;
         }
-        self
     }
 
     /// Reads current wave form state
